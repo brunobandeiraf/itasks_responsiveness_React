bf944ce45db54ce9156bbc35f3b2501875c470d6
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-10-13 18:56:24 -0300

    responsividade

--- src/components/Priorities/styles.js
+++ src/components/Priorities/styles.js
@@ -1,7 +1,8 @@
 import styled from "styled-components";
+import { DEVICE_BREAKPOINTS } from "../../styles/deviceBreakpoints";
 
 export const Container = styled.section`
-  padding: 32px;
+  padding: 2rem;
 `;
 
 export const Header = styled.header`
@@ -10,7 +11,7 @@ export const Header = styled.header`
   align-items: center;
 
   > h1 {
-    font-size: 24px;
+    font-size: 1.5rem;
     color: ${({ theme }) => theme.COLORS.RED_200};
     display: flex;
     align-items: center;
@@ -25,11 +26,18 @@ export const Header = styled.header`
 export const Tasks = styled.div`
   border: 1px dashed  ${({ theme }) => theme.COLORS.RED_200};
   min-height: 154px;
-  border-radius: 7px;
-  margin-top: 16px;
+  border-radius: 0.43rem;
+  margin-top: 1rem;
 
   display: flex;
-  gap: 16px;
+  gap: 2rem;
   padding: 24px;
   flex-wrap: wrap;
+
+  @media(max-width: ${DEVICE_BREAKPOINTS.MD}){
+    // centralizar as caixas
+    justify-content: center;
+    align-items: center;
+  }
+
 `;
